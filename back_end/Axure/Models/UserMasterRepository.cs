981441849f1238bccd47a029abed72806eb8fa72
Victor-Ciceia <53124867+Victor-Ciceia@users.noreply.github.com>
2020-02-21 13:55:34 -0300

    Se agrego autenticacion y base de datos local

---
+++ back_end/Axure/Models/UserMasterRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Axure.Models
+{
+    public class UserMasterRepository : IDisposable
+    {
+        AxureContext context = new AxureContext();
+
+        public User ValidateUser(string username, string password)
+        {
+            return context.Users.FirstOrDefault(user =>
+            user.UserName.Equals(username, StringComparison.OrdinalIgnoreCase)
+            && user.Password == password);
+        }
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+    }
+}
