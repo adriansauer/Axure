caf77179d5670446097cb94e76d556069a510b6c
adriansauer <60236462+adriansauer@users.noreply.github.com>
2020-03-12 12:25:30 -0300

    solucionar conflictos de merge

--- front_end/src/Componentes/Aside.js
+++ front_end/src/Componentes/Aside.js
@@ -2,6 +2,7 @@ import React,{Component} from 'react';
 import {connect} from 'react-redux';
 /**importando acciones */
 import {setSectionShow} from '../Redux/actions';
+import AsideProducto from './Modulo Productos/AsideProducto.js';
 import './style.css';
 class Aside extends Component{
 
@@ -10,12 +11,7 @@ class Aside extends Component{
         
         return(
             <div className="Aside btn-group-vertical col-md-2">
-  <button onClick={()=> this.props.setSectionShow(50)} type="button" className="btn btn-secondary btnAsideP">Stock</button>
-  <button onClick={()=> this.props.setSectionShow(51)} type="button" className="btn btn-secondary btnAsideP">Agregar Producto</button>
-  <button onClick={()=> this.props.setSectionShow(52)} type="button" className="btn btn-secondary btnAsideP">Generar orden de produccion</button>
-  <button onClick={()=> this.props.setSectionShow(53)} type="button" className="btn btn-secondary btnAsideP">Ordenes de produccion</button>
-  <button onClick={()=> this.props.setSectionShow(54)} type="button" className="btn btn-secondary btnAsideP1">Productos dados de baja</button>
-
+                    <AsideProducto/>
           </div>
         );
     }
