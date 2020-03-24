8915d96cc0fb8603b48ec9c0f24f77845fc64950
adriansauer <60236462+adriansauer@users.noreply.github.com>
2020-03-24 14:16:43 -0400

    actualizacion de la ui de stock

--- front_end/src/Axios/Api.js
+++ front_end/src/Axios/Api.js
@@ -1,10 +1,14 @@
 /**librerias de axios */
 import axios from 'axios';
-
 const requestHelper=axios.create(
     {
         baseURL:'http://localhost:53049/',
-       
+        /**  headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          }
+    */
+  
+     
     }
 )
 
@@ -16,16 +20,19 @@ export default {
 
         }),
         create:(data)=>requestHelper({
-            url:'Products',
+            url:'/Products/Create',
             method:'post',
-            data
+            data: data,
+            config: { headers: { "Content-Type": "multipart/form-data" } }
         }),
         delete:(id)=>requestHelper({
-            url:'Products/Delete/${id}',
-            method:'DELETE',
-        }),
+           
+            url:'Products/Delete/'+id,
+            method:'delete',
+        }
+        ),
         edit:(id,data)=>requestHelper({
-            url:'Products/Edit/'+{id},
+            url:'Products/Edit/'+id,
             method:'put',
             data
         })
