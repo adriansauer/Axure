262228b3902263c05a2902b568517eec4dc16eba
adriansauer <60236462+adriansauer@users.noreply.github.com>
2020-03-26 22:31:47 -0400

    actualizacion

--- front_end/src/Componentes/Modulo Productos/AsideProducto.js
+++ front_end/src/Componentes/Modulo Productos/AsideProducto.js
@@ -1,42 +1,58 @@
-import React,{Component} from 'react';
-import {connect} from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 /**importando acciones */
-import {setSectionShow} from '../../Redux/actions';
-import '../style.css';
-class AsideProducto extends Component{
-
-
-    render(){
-        
-        return(
-<<<<<<< HEAD
-<<<<<<< HEAD
-            <div className="Aside col-md-2">
-=======
-            <div className="Aside btn-group-vertical col-md-2">
->>>>>>> cce357a7a776ed6ebb792a9b1eaa8693dc784296
-=======
-
-            <div className="Aside btn-group-vertical col-md-2">
-
->>>>>>> 229a3ba99eaca9ff701551b287813d5ae20e8279
-  <button onClick={()=> this.props.setSectionShow(50)} type="button" className="btn btn-secondary btnAsideP">Stock</button>
-  <button onClick={()=> this.props.setSectionShow(51)} type="button" className="btn btn-secondary btnAsideP">Agregar Producto</button>
-  <button onClick={()=> this.props.setSectionShow(52)} type="button" className="btn btn-secondary btnAsideP">Generar orden de produccion</button>
-  <button onClick={()=> this.props.setSectionShow(53)} type="button" className="btn btn-secondary btnAsideP">Ordenes de produccion</button>
-  <button onClick={()=> this.props.setSectionShow(54)} type="button" className="btn btn-secondary btnAsideP1">Productos dados de baja</button>
-
-          </div>
-        );
-    }
+import { setSectionShow } from "../../Redux/actions";
+import "../style.css";
+class AsideProducto extends Component {
+  render() {
+    return (
+      <div className="Aside btn-group-vertical col-md-2">
+        <button
+          onClick={() => this.props.setSectionShow(50)}
+          type="button"
+          className="btn btn-secondary btnAsideP"
+        >
+          Stock
+        </button>
+        <button
+          onClick={() => this.props.setSectionShow(51)}
+          type="button"
+          className="btn btn-secondary btnAsideP"
+        >
+          Agregar Producto
+        </button>
+        <button
+          onClick={() => this.props.setSectionShow(52)}
+          type="button"
+          className="btn btn-secondary btnAsideP"
+        >
+          Generar orden de produccion
+        </button>
+        <button
+          onClick={() => this.props.setSectionShow(53)}
+          type="button"
+          className="btn btn-secondary btnAsideP"
+        >
+          Ordenes de produccion
+        </button>
+        <button
+          onClick={() => this.props.setSectionShow(54)}
+          type="button"
+          className="btn btn-secondary btnAsideP1"
+        >
+          Productos dados de baja
+        </button>
+      </div>
+    );
+  }
 }
-const mapStateToProps=state=>{
-    return{
-        codigo:state.sectionShow,
-    }
-}
-const mapDispatchToProps=({
-    setSectionShow,
-})
+const mapStateToProps = state => {
+  return {
+    codigo: state.sectionShow
+  };
+};
+const mapDispatchToProps = {
+  setSectionShow
+};
 
-export default connect(mapStateToProps,mapDispatchToProps)(AsideProducto);
+export default connect(mapStateToProps, mapDispatchToProps)(AsideProducto);
