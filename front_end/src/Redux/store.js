f2a3db9c774f219178de8eb542764bce1d30a211
adriansauer <60236462+adriansauer@users.noreply.github.com>
2020-03-26 22:19:28 -0400

    actualizacion de ui_stock

--- front_end/src/Redux/store.js
+++ front_end/src/Redux/store.js
@@ -6,6 +6,8 @@ import homeVisible from './Reducers/homeVisible.js';
 import materias_primas from './Reducers/materias_primas.js';
 import productos_terminados from './Reducers/productos_terminados.js';
 import productos_en_produccion from './Reducers/productos_en_produccion.js';
+import capitalTotal from './Reducers/capitalTotal.js';
+import capitalDeposito from './Reducers/capitalDeposito.js';
 
 const reducer=combineReducers({
    productos,
@@ -14,6 +16,8 @@ const reducer=combineReducers({
    productos_en_produccion,
    sectionShow,
    homeVisible,
+   capitalTotal,
+   capitalDeposito,
    
 });
 
