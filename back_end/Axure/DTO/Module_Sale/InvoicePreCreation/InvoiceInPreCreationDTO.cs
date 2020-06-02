ca42038d569a3007e8ab6e5c614ff4fd29dd8a43
VictorCiceia <victormciceia@gmail.com>
2020-06-01 23:07:56 -0400

    Se agrego un verificador para la creacion de una factura

---
+++ back_end/Axure/DTO/Module_Sale/InvoicePreCreation/InvoiceInPreCreationDTO.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Axure.DTO.Module_Sale.InvoicePreCreation
+{
+    public class InvoiceInPreCreationDTO
+    {
+        //Orden sale.
+        public int OrderSaleId { get; set; }
+
+        //Employee responsible for the sale.
+        public int EmployeeId { get; set; }
+
+        //Invoice sale condition. 
+        public string SaleCondition { get; set; }
+
+        //Order creation date.
+        public int Day { get; set; }
+        public int Month { get; set; }
+        public int Year { get; set; }
+
+        public List<ProductQuantityDTO> ListItems { get; set; }
+    }
+}
