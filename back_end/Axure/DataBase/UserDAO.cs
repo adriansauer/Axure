a3fd8f82e3b38aa255d5fcbd359c89696b68ab38
VictorCiceia <victormciceia@gmail.com>
2020-07-12 18:10:33 -0400

    Se agrego contolador de User

---
+++ back_end/Axure/DataBase/UserDAO.cs
@@ -0,0 +1,29 @@
+﻿using Axure.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Axure.DataBase
+{
+    public class UserDAO
+    {
+        public bool Edit(User u)
+        {
+            try
+            {
+                using (var db = new AxureContext())
+                {
+                    User userEdit = db.Users.FirstOrDefault(x => x.UserName.Equals(u.UserName));
+                    userEdit.Password = u.Password;
+                    db.SaveChanges();
+                    return false;
+                }
+            }
+            catch
+            {
+                return true;
+            }
+        }
+    }
+}
